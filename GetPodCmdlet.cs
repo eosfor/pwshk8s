d3325140c6aee22a94119eccab8a93098425e0f0
eosfor <eosfor@gmail.com>
2022-10-07 15:38:17 -0700

    init

---
+++ GetPodCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using k8s;
+using System.Management.Automation;
+
+
+namespace pwshk8s;
+
+[Cmdlet(VerbsCommon.Get, "K8sPod")]
+public class GetPodCmdlet: Cmdlet
+{
+    protected override void ProcessRecord()
+    {
+        //base.ProcessRecord();
+        var config = KubernetesClientConfiguration.BuildDefaultConfig();
+        IKubernetes client = new Kubernetes(config);
+        
+        var list = client.CoreV1.ListNamespacedPod("default");
+        
+        WriteObject(list);
+    }
+}
---
+++ GetSecretCmdlet.cs
@@ -0,0 +1,20 @@
+﻿using k8s;
+using System.Management.Automation;
+
+
+namespace pwshk8s;
+
+[Cmdlet(VerbsCommon.Get, "K8sSecret")]
+public class GetSecretCmdlet: Cmdlet
+{
+    protected override void ProcessRecord()
+    {
+        //base.ProcessRecord();
+        var config = KubernetesClientConfiguration.BuildDefaultConfig();
+        IKubernetes client = new Kubernetes(config);
+        
+        var list = client.CoreV1.ListNamespacedSecret("default");
+        
+        WriteObject(list);
+    }
+}
